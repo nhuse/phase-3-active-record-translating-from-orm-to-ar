103ee9929f9c7575fbfe861884d3112694951667
nhuse <nathan.huse@yahoo.com>
2021-08-15 17:33:00 -0500

    commit tree a656d0b6e02224df14a478c39b70feba9e2de6b8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
-class Dog
+require 'pry'
+
+class Dog < ActiveRecord::Base
+    # attr_accessor :name, :breed, :id
+
+    # def initialize(name:, breed:, id: nil)
+    #     @name = name
+    #     @breed = breed
+    #     @id = id
+    # end
+
+    # def create(name:, breed:)
+    #     puts name:
+    #     dog = Dog.new(name: name, breed: breed)
+    #     dog.save
+    # end
+
+    # def save
+    #     # Dog.create(name: name, breed: breed)
+    # end
 end
